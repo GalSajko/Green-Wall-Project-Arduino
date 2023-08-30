d713ecaec179827d17a65ffb5015ee3c787e3e15
GalSajko <gal.sajko@gmail.com>
2023-08-30 11:23:34 +0200

    Changed breaks servos values.

--- water_pump_bno_controll/water_pump_bno_controll.ino
+++ water_pump_bno_controll/water_pump_bno_controll.ino
@@ -19,11 +19,11 @@ int TUBE_HOLDER_SERVO = 11;
 
 /* Servo break MIN/MAX position */
 int MIN_MAX_VALUES[5][2] = {
-	{ 900, 2100 },
-	{ 900, 2100 },
-	{ 900, 2100 },
-	{ 900, 2100 },
-  { 900, 2100 },
+	{ 850, 2100 },
+	{ 850, 2100 },
+	{ 850, 2100 },
+	{ 850, 2100 },
+  { 850, 2100 },
 };
 
 int EXPANDED_TUBE_HOLDER_VALUE = 2875;
